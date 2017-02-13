d81fc682a463daf84b8104da504667e99298cceb
Leon Do <leondo123@gmail.com>
2017-02-13 16:23:54 -0500

    working for 1 message

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-// max 1 hit per second
-var tokens = require('./tokens.js')
+var Twitter = require('twitter');
 var AYLIENTextAPI = require('aylien_textapi');
+var tokens = require('./tokens.js')
+
+// =======================TOKENS==================================
+
+var client = new Twitter({
+  consumer_key: tokens.consumer_key,
+  consumer_secret: tokens.consumer_secret,
+  access_token_key: tokens.access_token_key,
+  access_token_secret: tokens.access_token_secret
+});
+ 
 
 var textapi = new AYLIENTextAPI({
   application_id: tokens.application_id,
   application_key: tokens.application_key
 });
 
-var url = 'https://en.wikipedia.org/wiki/Fall_of_man'
 
-textapi.sentiment({
-  'text': 'Bitcoin dives below $1,000 bad'
-}, function(error, response) {
-	console.log(response.polarity);
-	console.log(response.polarity_confidence);
 
-	var polarity = response.polarity
-	var polarityConfidence = response.polarity_confidence
+// =======================TWITTER==================================
+
+
+//var used to collect the stream
+var tweet;
+
+
+// get twitter stream
+var stream = client.stream('statuses/filter', {track: 'bitcoin'});
+stream.on('data', function(event) {
+	tweet = event.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').replace(/#/g, "").toString()
 });
+
+
+//send array every 30 sends
+setInterval(function(){
+	//console.log(tweet)
+	analyzeText(tweet, "bitcoin")
+}, 2000)
+
+
+
+
+
+// =======================AYLIEN==================================
+
+// passes text and coin to Aylien
+function analyzeText(text, coin){
+	textapi.sentiment({
+	  'text': text
+	}, function(error, response) {
+		console.log(response)
+		if (response.polarity !== 'neutral'){
+			tradeCrypto(response.polarity, coin)
+		}
+	});	
+}
+
+function tradeCrypto(polarity, coin){
+	console.log("trade " + polarity)
+	console.log("trade " + coin)
+}
+
